a5ee2f9c81b7daf21f48c57469e9ddf2da9d6f8c
龙露 <longlu@lpcollege.com>
2022-04-13 23:31:15 +0800

    fix last commit

--- docs/pages/agorithms/sidebar.js
+++ docs/pages/agorithms/sidebar.js
@@ -1,8 +1,7 @@
 module.exports ={
-    '/pages/browser/' : [
+    '/pages/agorithms/' : [
         { title:'排序算法',path:'/pages/agorithms/sort.md'},
         { title:'双指针',path:'/pages/agorithms/doublueP.md'},
         { title:'tire树',path:'/pages/agorithms/tire.md'},
-
       ]
 }
